9d58ee549763c62622b62f4dd0b5bdeb82bff9d1
Ana <dns80791@gmail.com>
2024-05-06 16:54:54 +0800

    Добавьте файлы проекта.

---
+++ Lab6Particles/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6Particles
+{
+    public partial class Form1 : Form
+    {
+
+        List<Particle> particles = new List<Particle>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+           
+        }
+
+        private int MousePositionX = 0;
+        private int MousePositionY = 0;
+
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            MousePositionX = e.X;
+            MousePositionY = e.Y;
+        }
+        private void UpdateState()
+        {
+            foreach (var particle in particles)
+            {
+                particle.Life -= 1;
+
+                if (particle.Life < 0)
+                {
+                    particle.Life = 20 + Particle.random.Next(100);
+
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+
+                    particle.Direction = Particle.random.Next(360);
+                    particle.Speed = Particle.random.Next(10) + 1;
+                    particle.Radius = Particle.random.Next(10) + 2;
+                }
+                else
+                {
+                    var directionInRadians = particle.Direction / 180 * Math.PI;
+                    particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
+                    particle.Y += (float)(particle.Speed * Math.Sin(directionInRadians));
+
+                }
+
+            }
+
+            for (var i = 0; i < 10; ++i)
+            {
+                if (particles.Count < 500)
+                {
+                    var particle = new Particle();
+                    particle.X = MousePositionX; 
+                    particle.Y = MousePositionY;   
+                    particles.Add(particle);
+                }
+                else
+                {
+                    break;
+                }
+                
+            }
+        }
+
+        private void Render(Graphics g)
+        {
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
+
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateState();
+
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.White);
+                Render(g);
+
+            }
+
+            picDisplay.Invalidate();    
+        }
+
+        
+    }
+}
---
+++ Lab6Particles/Particle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6Particles
+{
+    internal class Particle
+    {
+        public int Radius;
+        public float X;
+        public float Y;
+
+        public float Direction;
+        public float Speed;
+        public float Life;
+
+        public static Random random = new Random();
+
+        public Particle()
+        {
+            Direction = random.Next(360);
+            Speed = random.Next(10) + 1;
+            Radius = random.Next(10) + 2;
+            Life = random.Next(100) + 20;
+        }
+
+        public void Draw(Graphics g)
+        {
+            float k = Math.Min(1f, Life / 100);
+
+            int alpha = (int)(k * 255);
+
+            var color = Color.FromArgb(alpha, Color.Black);
+            
+            var b = new SolidBrush(color);
+
+            g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+
+            b.Dispose();
+        }
+    }
+}
